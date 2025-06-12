d9eec4762c033e4101bd2f38893d59876813cbe9
Tanish0x <kumvij69@gmail.com>
2025-06-12 10:26:07 +0530

    Update layout and install dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Mango',
+  description: 'Made by Alex',
+  generator: 'ALEX',
 }
 
 export default function RootLayout({
